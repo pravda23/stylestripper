b2d51d77e8453e912e3053d1343bb088ade90394
John Bartmann <pravda23@gmail.com>
2025-08-14 16:02:38 +0200

    added countries field

--- prompts.js
+++ prompts.js
@@ -4,5 +4,7 @@ export default {
   intro:
     "output a 50-word summary of the article contents. return only the summary text.",
   body: "output the body of the article in clean html. convert all headings to sentence case",
-  sdgs: "output a list of all UN SDGs that are mentioned in the article. return only the list of SDGs",
+  sdgs: "output a list of all UN SDGs that are mentioned in the article including their number and title. return only the list of SDGs with no explainer text. If no specific SDGs are mentioned, return the closest match to the article topic.",
+  country:
+    "output a list of all countries that are mentioned in the article. return only the list of countries with no explainer text",
 };
