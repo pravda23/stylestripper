4a442b5b2b026f9e025a03dd74215a855b8db6ec
John Bartmann <pravda23@gmail.com>
2025-08-20 12:13:58 +0200

    added additional taxonomy terms

--- prompts.js
+++ prompts.js
@@ -3,8 +3,14 @@ export default {
     "change the case of the title to 'sentence case' so that only 1. proper nouns and 2. the first word after a colon starts with uppercase letters. omit any explainer text. return only the title.",
   intro:
     "output a 50-word summary of the article contents. return only the summary text.",
-  body: "output the body of the article in clean html. convert all headings to sentence case",
-  sdgs: "output a list of all UN SDGs that are mentioned in the article including their number and title. return only the list of SDGs with no explainer text. If no specific SDGs are mentioned, return the closest match to the article topic.",
+  sdg: "output a list of all UN SDGs that are mentioned in the article including their number and title. return only the list of SDGs with no explainer text. If no specific SDGs are mentioned, return the closest match to the article topic.",
+  theme:
+    "output a list of all themes from this list that are mentioned in the article: [Gender, Sport and refugees, Youth development, Disability, Health, Peacebuilding, Child protection, Economic development, Policy, Democracy, Disaster response, Environment and sustainable development, Not applicable, All topics, Other]",
+  targetGroup:
+    "output a list of all potential target groups that are mentioned in the article from this list: [Not applicable, All target groups, Academics, Adults, Athletes, Children, Displaced people, Girls and women, LGBTQI+, People with disabilities, Policymakers, Practitioners, Seniors, Volunteers, Youth, Other]",
+  sport:
+    "output a list of all sports that are mentioned in the article. including physical activities such as walking, dancing and esports in the list of candidates to output. return only the list of sports with no explainer text. If no specific sports are mentioned, return 'All sports'.",
   country:
     "output a list of all countries that are mentioned in the article. return only the list of countries with no explainer text",
+  body: "output the body of the article in clean html. convert all headings to sentence case",
 };
